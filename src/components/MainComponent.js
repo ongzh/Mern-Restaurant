b6dd2bfd5bf396bb9f9f05ae0df3d9755e0f6fa5
ong <ong2323@gmail.com>
2021-08-04 12:18:10 +0800

    fixed bugs

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -67,13 +67,13 @@ class Main extends Component {
     const HomePage = () =>{
         return(
             <Home 
-            dish={this.props.dishes.dishes.filter((dish)=>dish.featued)[0]}
+            dish={this.props.dishes.dishes.filter((dish)=>dish.featured)[0]}
             dishesLoading={this.props.dishes.isLoading}
             dishesErrMess = {this.props.dishes.errMess}
-            promotion={this.props.promotions.promotions.filter((promo)=>promo.featued)[0]}
+            promotion={this.props.promotions.promotions.filter((promo)=>promo.featured)[0]}
             promosLoading={this.props.promotions.isLoading}
             promosErrMess = {this.props.promotions.errMess}
-            leader={this.props.leaders.leaders.filter((leader)=>leader.featued)[0]}
+            leader={this.props.leaders.leaders.filter((leader)=>leader.featured)[0]}
             leadersLoading = {this.props.leaders.isLoading}
             leadersErrMess = {this.props.leaders.errMess}
             />
